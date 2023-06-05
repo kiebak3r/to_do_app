4f225045b24fd3bdf0c935adb80d2447d96e0d67
kie <113192732+kiebak3r@users.noreply.github.com>
2023-06-05 23:23:24 +0100

    Update compile_windows.bat

--- compile_windows.bat
+++ compile_windows.bat
@@ -1,16 +1,37 @@
 @echo off
 
-REM Set variable data 
+REM Set variable data
 set APP=to_do_app.py
 set NAME="To Do App"
 set VERSION="1.0"
 set DESCRIPTION="To Do List"
 set COPYRIGHT="kiesus"
 
-REM Compile Command 
+REM Compile Command
 flet pack %APP% ^
   --product-name=%NAME% ^
   --product-version=%VERSION% ^
   --file-description=%DESCRIPTION% ^
   --copyright=%COPYRIGHT%
-  
+
+REM Check if the compilation is successful
+if %errorlevel% equ 0 (
+  echo Compilation completed.
+
+  REM Ask user to launch compiled file
+  set /p launch=Do you want to launch the compiled file? (Y/N): 
+
+  REM Check user's input
+  if /i "%launch%"=="Y" (
+    REM Launch the compiled file
+    start dist\to_do_app.exe
+  ) else if /i "%launch%"=="N" (
+    REM Do nothing
+  ) else (
+    REM Invalid input
+    echo Invalid input. No action taken.
+  )
+) else (
+  echo Compilation failed.
+)
+
